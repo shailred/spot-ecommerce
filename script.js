ac8a59c051848d51f8efa6544cd5fb6b07146c1e
shailred <shailendrareddy123@gmail.com>
2024-05-16 18:01:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const imgs= document.querySelectorAll('.header-slider ul img');
+const prev_btn=document.querySelector('.controls_prev');
+const next_btn=document.querySelector('.controls_next');
+let n = 0;
+function changeSlide(){
+ for (let i=0; i< imgs.length;i++) {
+    imgs [i].style.display='none';
+ }  
+  imgs[n].style.display='block';
+}
+changeSlide ();
+prev_btn.addEventListener ('click'.(et)=>{(if n>0) {n--;}else {n=imgs.length-1;}
+changeSlide ();
+
+   })
+
+ const scrollContainer = document.querySelectorAll ('. products');
+ for (const item of scrollContainer){ 
+ item.addEventListener ('wheel', (evt)=> {
+ evt.preventDefault ();
+ item.scrollLeft+= evt.deltaY;
+     });
+   }
